60c7755f25514649046616e48748b5d08ac84893
Pedro Sales Santos <psalessa@everis.com>
2020-08-22 03:06:09 -0300

    .vue changes

--- vue-cli/simple-project/src/App.vue
+++ vue-cli/simple-project/src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    {{ title }}
+    <button @click="title += '#'">Change</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data: function () {
+    return {
+      title: "VueJS data test",
+    };
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: green;
+  color: #fff;
 }
 </style>
