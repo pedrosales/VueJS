421399043cdbfea866f42eeb2038f4b2de9cd301
Pedro Sales Santos <psalessa@everis.com>
2020-08-22 13:09:32 -0300

    user list and user details

--- components/components-comunication-test/src/App.vue
+++ components/components-comunication-test/src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <user-list :users="users" />
+    <user-details />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserList from "./components/UserList";
+import UserDetails from "./components/UserDetails";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  components: { UserList, UserDetails },
+  data() {
+    return {
+      users: [
+        { id: 1, name: "Ana", age: 31 },
+        { id: 2, name: "Carlos", age: 22 },
+        { id: 3, name: "Daniel", age: 40 },
+        { id: 4, name: "Ema", age: 17 },
+        { id: 5, name: "Lia", age: 28 },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
@@ -24,5 +34,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  display: flex;
+  font-size: 2rem;
+}
+
+#app div {
+  margin: 20px;
 }
 </style>
