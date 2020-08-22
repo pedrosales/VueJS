f495ad70f412ab82b33e2b827e5627d551fdb092
Pedro Sales Santos <psalessa@everis.com>
2020-08-22 08:28:35 -0300

    components intro

---
+++ components/components-intro/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>Counters</h1>
+    <app-counter></app-counter>
+    <app-counter></app-counter>
+    <app-counter></app-counter>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ components/components-intro/src/Counter.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="counter">
+    <span>{{ counter }}</span>
+    <button @click="add">+</button>
+    <button @click="sub">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.counter++;
+    },
+    sub() {
+      this.counter--;
+    },
+  },
+};
+</script>
+
+<style>
+.counter span {
+  border-bottom: 1px solid #ccc;
+  height: 30px;
+  padding: 5px 25px;
+}
+
+.counter button {
+  height: 30px;
+  width: 30px;
+  border-radius: 15px;
+  background-color: coral;
+  color: #fff;
+  margin-left: 10px;
+  outline: none;
+}
+</style>
