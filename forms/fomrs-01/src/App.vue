2b9c3342c1d6a9934edc12a280c2aa103ec3977d
Pedro Sales Santos <psalessa@everis.com>
2020-08-23 18:25:59 -0300

    inicial project forms

--- forms/fomrs-01/src/App.vue
+++ forms/fomrs-01/src/App.vue
@@ -1,10 +1,146 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <h1>Register you claim</h1>
+    <div class="content">
+      <form class="panel">
+        <div class="header">Form</div>
+        <Label name="E-mail">
+          <input type="text" />
+        </Label>
+        <Label name="Password">
+          <input type="password" />
+        </Label>
+        <Label name="Age">
+          <input type="number" />
+        </Label>
+        <Label name="Message">
+          <textarea name cols="30" rows="5"></textarea>
+        </Label>
+        <Label name="Problem specs">
+          <span class="mr-4">
+            <input type="checkbox" value="reproducible" /> Reproducible
+          </span>
+          <span>
+            <input type="checkbox" value="intermitent" /> Intermitent
+          </span>
+        </Label>
+        <Label name="Witch Product">
+          <span class="mr-4">
+            <input type="radio" /> Web
+          </span>
+          <span class="mr-4">
+            <input type="radio" /> Mobile
+          </span>
+          <span>
+            <input type="radio" /> Other
+          </span>
+        </Label>
+        <Label name="Priority">
+          <select name id>
+            <option></option>
+          </select>
+        </Label>
+        <Label name="First claim?">
+          <SwitchSelector />
+        </Label>
+        <hr />
+        <button>Send</button>
+      </form>
+      <div class="panel">
+        <div class="header">Result</div>
+        <Label name="E-mail">
+          <span>???</span>
+        </Label>
+        <Label name="Password">
+          <span>???</span>
+        </Label>
+        <Label name="Age">
+          <span>???</span>
+        </Label>
+        <Label name="Message">
+          <span>???</span>
+        </Label>
+        <Label name="Choice the options">
+          <span>???</span>
+        </Label>
+        <Label name="Wich product?">
+          <span>???</span>
+        </Label>
+        <Label name="Priority">
+          <span>???</span>
+        </Label>
+        <Label name="First claim?">
+          <span>???</span>
+        </Label>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import SwitchSelector from "./components/SwitchSelector.vue";
+import Label from "./components/Label.vue";
+
+export default {
+  name: "app",
+  components: { SwitchSelector, Label },
+};
 </script>
 
 <style>
+body {
+  background-color: #ececec;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+
+  display: flex;
+  flex-direction: column;
+}
+
+.content {
+  display: flex;
+}
+
+.panel {
+  flex: 1;
+  background-color: #fff;
+  margin: 0px 10px;
+  padding: 20px;
+  border: 1px solid #aaa;
+  border-radius: 5px;
+}
+
+.panel .header {
+  width: 100%;
+  background-color: #ddd;
+  padding: 10px 0px;
+  border-radius: 5px;
+  font-size: 1.4rem;
+}
+
+#app form button {
+  float: right;
+  margin: 10px 0px;
+  padding: 10px 20px;
+  font-size: 1.4rem;
+  border-radius: 5px;
+  color: #fff;
+  background-color: #2196f3;
+}
+
+#app h1 {
+  font-weight: 200;
+  margin: 20px;
+  padding: 0;
+}
+
+.mr-4 {
+  margin-right: 40px;
+}
 </style>
---
+++ forms/fomrs-01/src/components/SwitchSelector.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="switch" @click="on = !on" :class="{on, off: !on }">
+    <div v-if="on" class="button"></div>
+    <div v-else class="button"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      on: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.switch {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 15px;
+  width: 40px;
+  border-radius: 20px;
+  border: 1px solid #aaa;
+}
+
+.button {
+  position: absolute;
+  margin: 0px 5px;
+  height: 25px;
+  width: 25px;
+  border-radius: 14px;
+  background-color: #fff;
+  box-shadow: 0px 0px 5px #000b;
+}
+
+.off {
+  background-color: #aaa;
+}
+
+.on {
+  background-color: #7495c2;
+}
+
+.off .button {
+  left: -10px;
+}
+
+.on .button {
+  background-color: #255ca9;
+  align-self: flex-end;
+}
+</style>
