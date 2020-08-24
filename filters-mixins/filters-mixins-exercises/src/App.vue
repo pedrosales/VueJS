47351aa4d9a73ded4ee08dfd4389c96fa063bb74
Pedro Sales Santos <psalessa@everis.com>
2020-08-24 01:28:20 -0300

    filters and mixins exercises

---
+++ filters-mixins/filters-mixins-exercises/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>Filters and Mixins</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 2.4rem;
+}
+</style>
