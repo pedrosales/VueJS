7a8b921942dcc8ac22717f717b827cfa36318c45
Pedro Sales Santos <psalessa@everis.com>
2020-08-24 00:29:56 -0300

    directives test

---
+++ directives/directives-test/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>Directives (Test)</h1>
+    <hr />
+    <!-- Exercício -->
+    <!-- Escreva uma diretiva que funcione com o v-on (escute eventos) -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+button {
+  margin: 10px 0px;
+  padding: 10px 20px;
+  font-size: 1.4rem;
+  border-radius: 5px;
+  color: #fff;
+  background-color: #2196f3;
+}
+</style>
