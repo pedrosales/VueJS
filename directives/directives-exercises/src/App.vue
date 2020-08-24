9ec5377751edf034578a6399385e963db604e41d
Pedro Sales Santos <psalessa@everis.com>
2020-08-23 23:55:04 -0300

    local custom directive

--- directives/directives-exercises/src/App.vue
+++ directives/directives-exercises/src/App.vue
@@ -7,7 +7,9 @@
     <hr />
     <p v-spotlight:background.delay="'lightblue'">Custom directive</p>
     <p v-spotlight.delay="color">Custom directive</p>
-
+    <hr />
+    <p v-localspotlight:background.delay="'red'">Local custom directive</p>
+    <p v-localspotlight.delay="color">Local custom directive</p>
     <!-- <p v-test:args.modifier1.modifier2.modifier3="value"></p> -->
   </div>
 </template>
@@ -15,6 +17,24 @@
 <script>
 export default {
   name: "App",
+  directives: {
+    localspotlight: {
+      bind(el, binding) {
+        // el.style.backgroundColor = "lightgreen"
+        let delay = 0;
+
+        if (binding.modifiers["delay"]) delay = 3000;
+
+        setTimeout(() => {
+          if (binding.arg === "background") {
+            el.style.backgroundColor = binding.value;
+          } else {
+            el.style.color = binding.value;
+          }
+        }, delay);
+      },
+    },
+  },
   data() {
     return {
       color: "blue",
