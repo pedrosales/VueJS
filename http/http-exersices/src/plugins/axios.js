d5b5ac7add098e1e80649e027b5c0b6809e1b5b4
Pedro Sales Santos <psalessa@everis.com>
2020-08-29 00:19:32 -0300

    global headers

--- http/http-exersices/src/plugins/axios.js
+++ http/http-exersices/src/plugins/axios.js
@@ -1,13 +1,18 @@
 import Vue from "vue";
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://vue-backend-24531.firebaseio.com/";
+// // axios.defaults.baseURL = "https://vue-backend-24531.firebaseio.com/";
+// axios.defaults.headers.common["Authorization"] = "abc123";
+// axios.defaults.headers.get["Accepts"] = "application/json";
 
 Vue.use({
     install(Vue) {
         // Vue.prototype.$http = axios;
         Vue.prototype.$http = axios.create({
-            baseURL: "https://vue-backend-24531.firebaseio.com/"
+            baseURL: "https://vue-backend-24531.firebaseio.com/",
+            headers: {
+                "Authorization": "abc123"
+            }
         });
 
         Vue.prototype.$http.interceptors.request.use(config => {
