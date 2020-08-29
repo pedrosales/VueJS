a78ec91bdcfafedf3227aadec95737be0ea91361
Pedro Sales Santos <psalessa@everis.com>
2020-08-29 01:05:31 -0300

    vue router project

---
+++ vue-router/routes-exercises/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>Routes with VueRouter</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
