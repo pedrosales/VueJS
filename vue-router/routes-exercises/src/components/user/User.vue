f2b7f6b86612a4ad62159ecccb2fbee76ea6baaa
Pedro Sales Santos <psalessa@everis.com>
2020-08-29 01:57:11 -0300

    navigate JS

--- vue-router/routes-exercises/src/components/user/User.vue
+++ vue-router/routes-exercises/src/components/user/User.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="user">
     <h2>Users</h2>
+    <hr />
+    <button success @click="backIndex">Return</button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    backIndex() {
+      //   this.$router.push("/");
+      this.$router.push({ path: "/" });
+    },
+  },
+};
 </script>
 
 <style>
