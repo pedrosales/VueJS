c4cadb82dcbdb00d45a9404e367114cc63d671c8
Pedro Sales Santos <psalessa@everis.com>
2020-08-29 08:38:46 -0300

    navigation user details

--- vue-router/routes-exercises/src/components/user/UserList.vue
+++ vue-router/routes-exercises/src/components/user/UserList.vue
@@ -2,9 +2,9 @@
   <div class="user-list">
     <h3>User List</h3>
     <ul class="list">
-      <li class="list-item">User 1</li>
-      <li class="list-item">User 2</li>
-      <li class="list-item">User 3</li>
+      <router-link tag="li" to="/users/1" class="list-item">User 1</router-link>
+      <router-link tag="li" to="/users/2" class="list-item">User 2</router-link>
+      <router-link tag="li" to="/users/3" class="list-item">User 3</router-link>
     </ul>
   </div>
 </template>
