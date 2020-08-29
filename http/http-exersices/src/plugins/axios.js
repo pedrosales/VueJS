6da07c8f497375d549b33f4b08c2e2549002924d
Pedro Sales Santos <psalessa@everis.com>
2020-08-28 23:37:35 -0300

    axios instance

--- http/http-exersices/src/plugins/axios.js
+++ http/http-exersices/src/plugins/axios.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://vue-backend-24531.firebaseio.com/";
+// axios.defaults.baseURL = "https://vue-backend-24531.firebaseio.com/";
 
 Vue.use({
     install(Vue) {
-        Vue.prototype.$http = axios;
+        // Vue.prototype.$http = axios;
+        Vue.prototype.$http = axios.create({
+            baseURL: "https://vue-backend-24531.firebaseio.com/"
+        });
     }
 });
